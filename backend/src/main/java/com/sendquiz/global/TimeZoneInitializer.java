db772a92edc2ca25340dd5cbdfeab246e8853afa
Jung-YouHwan <yhwjd99@gmail.com>
2023-06-01 23:08:27 +0900

    fixed untracked files

--- backend/src/main/java/com/sendquiz/global/TimeZoneInitializer.java
+++ backend/src/main/java/com/sendquiz/global/TimeZoneInitializer.java
@@ -5,13 +5,11 @@ import org.springframework.stereotype.Component;
 
 import java.util.TimeZone;
 
-import static com.sendquiz.global.constant.CommonConstant.ASIA_SEOUL;
-
 @Component
 public class TimeZoneInitializer {
 
     @PostConstruct
     public void initialize() {
-        TimeZone.setDefault(TimeZone.getTimeZone(ASIA_SEOUL));
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 }
